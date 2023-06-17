3746dbd740791d82882f35b04137d66a9198a683
Artem Fedorov <fyod-artem@mail.ru>
2023-06-17 20:16:17 +0200

    Сделал домашнее задание по 3 семинару

--- Homework 3/Task 19/Program.cs
+++ Homework 3/Task 19/Program.cs
@@ -3,3 +3,25 @@
 // 14212 -> нет
 // 23432 -> да
 // 12821 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Данное число не является пятизначным");
+}
+
+int firstDigit = number / 10000;
+int secondDigit = number / 1000 % 10;
+int fourthDigit = number / 10 % 10;
+int fifthDigit = number % 10;
+
+if (firstDigit == fifthDigit && secondDigit == fourthDigit)
+{
+    Console.WriteLine("Данное пятизначное число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Данное пятизначное число НЕ является палиндромом");
+}
--- Homework 3/Task 21/Program.cs
+++ Homework 3/Task 21/Program.cs
@@ -3,4 +3,31 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+Console.WriteLine("Введите координату первой точки по оси X");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату первой точки по оси Y");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату первой точки по оси Z");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату второй точки по оси X");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату второй точки по оси Y");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату второй точки по оси Z");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+// Math.Pow(x, 2) - возведение в квадрат
+// Math.Sqrt(x) - извлечение квадратного корня
+
+double skobX = Math.Pow (x1 - x2, 2);
+double skobY = Math.Pow (y1 - y2, 2);
+double skobZ = Math.Pow (z1 - z2, 2);
+
+double result = Math.Sqrt(skobX + skobY + skobZ);
+Console.WriteLine("Расстояние между двумя точками = " + $"{result:f2}");
 
