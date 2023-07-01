1db125f7113e40128687062df7deeb1985868778
Artem Fedorov <fyod-artem@mail.ru>
2023-07-02 01:06:21 +0200

    Закончена домашняя работа по 7 семинару

--- Homework 7/Task 52/Program.cs
+++ Homework 7/Task 52/Program.cs
@@ -42,6 +42,6 @@ double delitel = 3.0;
 double sum1 = (myMatrix[0, 0] + myMatrix[1, 0] + myMatrix[2, 0]) / delitel;
 double sum2 = (myMatrix[0, 1] + myMatrix[1, 1] + myMatrix[2, 1]) / delitel;
 double sum3 = (myMatrix[0, 2] + myMatrix[1, 2] + myMatrix[2, 2]) / delitel;
-double sum4 = (myMatrix[0, 3] + myMatrix[1, 3] + myMatrix[2, 3]) / decimalelitel;
+double sum4 = (myMatrix[0, 3] + myMatrix[1, 3] + myMatrix[2, 3]) / delitel;
 
 Console.WriteLine($"Среднее арифметическое каждого столбца {sum1:f1}, {sum2:f1}, {sum3:f1}, {sum4:f1}");
